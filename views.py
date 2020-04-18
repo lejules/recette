a995ed28da928a8122d95c7f67109e75a795f015
lejules <collegerb@free.fr>
2020-04-18 19:55:15 +0200

    Ajout du css et du template

--- views.py
+++ views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    info = 'Coucou qui voilou ?'
+    return render(request, 'recette/index.html', {'info': info})

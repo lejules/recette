e013dac62970f9d88a66c41cf4bda51db63fadd0
lejules <collegerb@free.fr>
2020-04-20 12:13:32 +0200

    Ajout du template de base et ajout formulaire Media sur recette

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Recette
+from .models import Recette, Media, Ingredient
 
 
 class formRecette(forms.ModelForm):
@@ -7,3 +7,17 @@ class formRecette(forms.ModelForm):
     class Meta:
         model = Recette
         exclude = ['nombre_visite', 'utilisateur']
+
+
+class formMedia(forms.ModelForm):
+
+    class Meta:
+        model = Media
+        exclude = ['recette']
+
+
+class formIngredient(forms.ModelForm):
+
+    class Meta:
+        model = Ingredient
+        exclude = ['recette']
--- views.py
+++ views.py
@@ -1,27 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Recette, Media
-
+from django.forms import formset_factory
+from  django.urls import reverse
 from django.shortcuts import render, HttpResponseRedirect
-from .forms import formRecette
+from .forms import formRecette, formMedia
 
 
 # Create your views here.
 def index(request):
     recettes = Recette.objects.all()[:6]
-    return render(request, 'recette/index.html', {'recettes': recettes})
-    info = 'salut les terriens'
-    return render(request, 'recette/index.html', {'info': info})
+    mot = 'Très bien, aucune erreur à afficher'
+    return render(request, 'recette/index.html', {'recettes': recettes, 'mot':mot})
 
-
+@login_required
 def ajout_recette(request) :
+    # On associe 2 formulaires dans un seul avec un préfixe
+    # pour éviter les confusions de champs
+    # RecetteFormSet = formset_factory(formRecette)
+    # MediaFormSet = formset_factory(formMedia)
     if request.method == 'POST':
-        form = formRecette(request.POST)
-        if form.is_valid():
-            recette = form.save(commit=False)
+        #form = formRecette(request.POST)
+        recette_formset = formRecette(request.POST, prefix='recette')
+        media_formset = formMedia(request.POST, request.FILES, prefix='media')
+        if recette_formset.is_valid() and media_formset.is_valid():
+            recette = recette_formset.save(commit=False)
             recette.utilisateur_id = request.user.id
             recette.save()
-            return HttpResponseRedirect('')
+            media = media_formset.save(commit=False)
+            media.recette_id = recette.id
+            media.save()
+            return HttpResponseRedirect(reverse('recette:index'))
+        else:
+            recettes = Recette.objects.all()[:6]
+            mot = 'Erreur : '+str(media_formset.is_valid())
+            return render(request, 'recette/index.html', {'recettes': recettes, 'mot': mot})
+
     else:
-        form = formRecette()
-        return render(request, 'recette/ajout_recette.html', {'form':form})
+        #form = formRecette()
+        # recette_formset = RecetteFormSet(prefix='recette')
+        # media_formset = MediaFormSet(prefix='media')
+        #return render(request, 'recette/ajout_recette.html', {'form':form})
+        recette_formset = formRecette(prefix='recette')
+        media_formset = formMedia(prefix='media')
+    return render(request, 'recette/ajout_recette.html',
+                      {'recette_formset': recette_formset, 'media_formset':media_formset})
 
